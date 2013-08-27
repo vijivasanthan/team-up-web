8a0d285e193efb184f7b77063b39ead08eaeba90
Cengizism <culusoy@ask-cs.com>
2013-08-27 15:22:19 +0200

    seperating data&options from treegrid constructor

--- war/js/controllers/treegrid.js
+++ war/js/controllers/treegrid.js
@@ -11,55 +11,80 @@ angular.module('WebPaige.Controllers.TreeGrid', [])
       '$rootScope', '$scope', '$window',
       function ($rootScope, $scope, $window)
       {
+        /**
+         * TreeGrid
+         */
+        $scope.treegrid = {
 
+          /**
+           * Options
+           */
+          options: {
+            width: 'auto',
+            height: $('#wrap').height() - (270 + 200) + 'px'
+          },
 
-        function drawGrid()
-        {
-          var treeGridOptions = {
-              width: 'auto',
-              height: $('#wrap').height() - (270 + 200) + 'px'
-            },
-            filesContainer 	= document.getElementById('files'),
-            filesOptions 		= {
-              columns: [
-                {name: 'name', text: 'Name', title: 'Name of the files'},
-                {name: 'size', text: 'Size', title: 'Size of the files in kB (kilo bytes)'},
-                {name: 'date', text: 'Date', title: 'Date the file is last updated'}
-              ],
-              dataTransfer: {
-                allowedEffect: 	'move',
-                dropEffect: 		'none'
+          /**
+           * Build TreeGrid
+           */
+          build: function (id, data, options)
+          {
+            new links.TreeGrid(id, this.options)
+              .draw(new links.DataTable(data, options));
+          },
+
+          /**
+           * Init TreeGrid
+           */
+          init: function ()
+          {
+            this.build(
+              document.getElementById('left'),
+              $scope.files,
+              {
+                columns: [
+                  {name: 'name', text: 'Name', title: 'Name of the files'},
+                  {name: 'size', text: 'Size', title: 'Size of the files in kB (kilo bytes)'},
+                  {name: 'date', text: 'Date', title: 'Date the file is last updated'}
+                ],
+                dataTransfer: {
+                  allowedEffect: 	'move',
+                  dropEffect: 		'none'
+                }
               }
-            },
-            filesTreeGrid 			= new links.TreeGrid(filesContainer, treeGridOptions),
-            filesDataConnector 	= new links.DataTable($scope.files, filesOptions),
-            foldersOptions = {},
-            foldersOptions = {
-              dataTransfer : {
-                allowedEffect: 	'move',
-                dropEffect: 		'move'
+            );
+
+            this.build(
+              document.getElementById('right'),
+              $scope.folders,
+              {
+                dataTransfer : {
+                  allowedEffect: 	'move',
+                  dropEffect: 		'move'
+                }
               }
-            },
-            foldersContainer 			= document.getElementById('folders'),
-            foldersDataConnector 	= new links.DataTable($scope.folders, foldersOptions),
-            foldersTreeGrid 			= new links.TreeGrid(foldersContainer, treeGridOptions);
+            );
+          }
 
-          filesTreeGrid.draw(filesDataConnector);
-          foldersTreeGrid.draw(foldersDataConnector);
-        }
+        };
 
 
-
-
-        $window.onresize = function () { drawGrid(); };
-
-
+        /**
+         * Draw treegrid
+         */
         setTimeout(function ()
         {
-          drawGrid();
+          $scope.treegrid.init();
         }, 100);
 
 
+        /**
+         * Attach listener for window resizing
+         */
+        $window.onresize = function ()
+        {
+          $scope.treegrid.init();
+        };
 
       }
     ]);
