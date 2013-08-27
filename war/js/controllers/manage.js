851c8a2cce18bdbae081393755dbf7299ddfcc5a
Cengizism <culusoy@ask-cs.com>
2013-08-27 09:47:42 +0200

    view switchers for manage page

--- war/js/controllers/manage.js
+++ war/js/controllers/manage.js
@@ -10,7 +10,38 @@ angular.module('WebPaige.Controllers.Manage', [])
  */
 .controller('manageCtrl',[
     '$rootScope', '$scope', '$location', 'Clients','data', '$route', '$routeParams', 'Storage','Teams',
-    function ($rootScope, $scope, $location, Clients,data, $route, $routeParams, Storage , Teams){
+    function ($rootScope, $scope, $location, Clients, data, $route, $routeParams, Storage , Teams){
+
+
+      /**
+       * View setter
+       */
+      function setView (hash)
+      {
+        $scope.views = {
+          teamClients:  false,
+          teams:        false,
+          clients:      false
+        };
+
+        $scope.views[hash] = true;
+      }
+
+
+      /**
+       * Switch between the views and set hash accordingly
+       */
+      $scope.setViewTo = function (hash)
+      {
+        $scope.$watch(hash, function ()
+        {
+          $location.hash(hash);
+
+          setView(hash);
+        });
+      };
+
+      $scope.setViewTo('teamClients');
         
     }
     
