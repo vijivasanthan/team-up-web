e2975a2b22ec588eba647a48e69a240a55e9dc5c
Cengizism <culusoy@ask-cs.com>
2013-09-09 11:49:04 +0200

    timeline work in progress

--- war/js/controllers/planboard.js
+++ war/js/controllers/planboard.js
@@ -9,9 +9,15 @@ angular.module('WebPaige.Controllers.Planboard', [])
  * Planboard controller
  */
   .controller('planboard',[
-    '$rootScope', '$scope', '$location', 'Dater',
-    function ($rootScope, $scope, $location, Dater)
+    '$rootScope', '$scope', '$location', 'Dater', 'Storage',
+    function ($rootScope, $scope, $location, Dater, Storage)
     {
+
+
+      console.log('clients ->', angular.fromJson(Storage.get('ClientGroups')));
+
+      $scope.teams = angular.fromJson(Storage.get('Teams'));
+
 
       /**
        * View setter
@@ -46,12 +52,37 @@ angular.module('WebPaige.Controllers.Planboard', [])
        */
       $scope.setViewTo('teams');
 
+      var data = {
+        "user": [
+          {
+            "count": 0,
+            "end": 1378681200,
+            "recursive": true,
+            "start": 1378504800,
+            "text": "com.ask-cs.State.Available",
+            "type": "availability",
+            "wish": 0
+          },
+          {
+            "count": 0,
+            "end": 1378850400,
+            "recursive": true,
+            "start": 1378720800,
+            "text": "com.ask-cs.State.Available",
+            "type": "availability",
+            "wish": 0
+          }
+        ],
+        "synced": 1378384322609,
+        "periods": {
+          "start": 1378332000000,
+          "end": 1378936800000
+        }
+      };
 
-      // var data = {"user":[{"count":0,"end":1378681200,"recursive":true,"start":1378504800,"text":"com.ask-cs.State.Available","type":"availability","wish":0},{"count":0,"end":1378850400,"recursive":true,"start":1378720800,"text":"com.ask-cs.State.Available","type":"availability","wish":0}],"groupId":"a2408ffc-69b5-1030-a3ab-005056bc7e66","aggs":[{"id":"a2408ffc-69b5-1030-a3ab-005056bc7e66","division":{"id":"knrm.StateGroup.BeschikbaarNoord","label":"Noord"},"data":[{"diff":10,"end":1378346400,"start":1378332000,"wish":1},{"diff":9,"end":1378351800,"start":1378346400,"wish":1},{"diff":8,"end":1378353600,"start":1378351800,"wish":1},{"diff":5,"end":1378357200,"start":1378353600,"wish":1},{"diff":3,"end":1378375200,"start":1378357200,"wish":1},{"diff":2,"end":1378389600,"start":1378375200,"wish":1},{"diff":3,"end":1378393200,"start":1378389600,"wish":1},{"diff":5,"end":1378396800,"start":1378393200,"wish":1},{"diff":6,"end":1378398600,"start":1378396800,"wish":1},{"diff":7,"end":1378400400,"start":1378398600,"wish":1},{"diff":10,"end":1378407600,"start":1378400400,"wish":1},{"diff":11,"end":1378429200,"start":1378407600,"wish":1},{"diff":10,"end":1378430100,"start":1378429200,"wish":1},{"diff":11,"end":1378432800,"start":1378430100,"wish":1},{"diff":10,"end":1378440000,"start":1378432800,"wish":1},{"diff":6,"end":1378443600,"start":1378440000,"wish":1},{"diff":4,"end":1378447200,"start":1378443600,"wish":1},{"diff":3,"end":1378450800,"start":1378447200,"wish":1},{"diff":4,"end":1378483200,"start":1378450800,"wish":1},{"diff":5,"end":1378485000,"start":1378483200,"wish":1},{"diff":6,"end":1378486800,"start":1378485000,"wish":1},{"diff":7,"end":1378494000,"start":1378486800,"wish":1},{"diff":8,"end":1378497600,"start":1378494000,"wish":1},{"diff":7,"end":1378530000,"start":1378497600,"wish":1},{"diff":6,"end":1378533600,"start":1378530000,"wish":1},{"diff":5,"end":1378569600,"start":1378533600,"wish":1},{"diff":4,"end":1378573200,"start":1378569600,"wish":1},{"diff":5,"end":1378598400,"start":1378573200,"wish":1},{"diff":4,"end":1378616400,"start":1378598400,"wish":1},{"diff":3,"end":1378634400,"start":1378616400,"wish":1},{"diff":4,"end":1378641600,"start":1378634400,"wish":1},{"diff":5,"end":1378659600,"start":1378641600,"wish":1},{"diff":6,"end":1378670400,"start":1378659600,"wish":1},{"diff":7,"end":1378677600,"start":1378670400,"wish":1},{"diff":8,"end":1378692000,"start":1378677600,"wish":1},{"diff":7,"end":1378699200,"start":1378692000,"wish":1},{"diff":4,"end":1378702800,"start":1378699200,"wish":1},{"diff":3,"end":1378706400,"start":1378702800,"wish":1},{"diff":2,"end":1378738800,"start":1378706400,"wish":1},{"diff":3,"end":1378740600,"start":1378738800,"wish":1},{"diff":4,"end":1378746000,"start":1378740600,"wish":1},{"diff":6,"end":1378749600,"start":1378746000,"wish":1},{"diff":7,"end":1378753200,"start":1378749600,"wish":1},{"diff":6,"end":1378760400,"start":1378753200,"wish":1},{"diff":9,"end":1378764000,"start":1378760400,"wish":1},{"diff":10,"end":1378778400,"start":1378764000,"wish":1},{"diff":9,"end":1378785600,"start":1378778400,"wish":1},{"diff":7,"end":1378789200,"start":1378785600,"wish":1},{"diff":3,"end":1378825200,"start":1378789200,"wish":1},{"diff":4,"end":1378827000,"start":1378825200,"wish":1},{"diff":5,"end":1378830600,"start":1378827000,"wish":1},{"diff":6,"end":1378832400,"start":1378830600,"wish":1},{"diff":9,"end":1378839600,"start":1378832400,"wish":1},{"diff":10,"end":1378850400,"start":1378839600,"wish":1},{"diff":11,"end":1378864800,"start":1378850400,"wish":1},{"diff":10,"end":1378872000,"start":1378864800,"wish":1},{"diff":7,"end":1378875600,"start":1378872000,"wish":1},{"diff":4,"end":1378879200,"start":1378875600,"wish":1},{"diff":3,"end":1378913400,"start":1378879200,"wish":1},{"diff":4,"end":1378915200,"start":1378913400,"wish":1},{"diff":6,"end":1378917000,"start":1378915200,"wish":1},{"diff":7,"end":1378918800,"start":1378917000,"wish":1},{"diff":10,"end":1378929600,"start":1378918800,"wish":1},{"diff":9,"end":1378936800,"start":1378929600,"wish":1}],"ratios":{"less":0,"even":0,"more":100},"durations":{"less":0,"even":0,"more":604800,"total":604800}},{"id":"a2408ffc-69b5-1030-a3ab-005056bc7e66","division":{"id":"knrm.StateGroup.BeschikbaarZuid","label":"Zuid"},"data":[{"diff":9,"end":1378346400,"start":1378332000,"wish":1},{"diff":8,"end":1378351800,"start":1378346400,"wish":1},{"diff":7,"end":1378353600,"start":1378351800,"wish":1},{"diff":4,"end":1378357200,"start":1378353600,"wish":1},{"diff":2,"end":1378389600,"start":1378357200,"wish":1},{"diff":3,"end":1378393200,"start":1378389600,"wish":1},{"diff":5,"end":1378396800,"start":1378393200,"wish":1},{"diff":6,"end":1378398600,"start":1378396800,"wish":1},{"diff":7,"end":1378400400,"start":1378398600,"wish":1},{"diff":10,"end":1378407600,"start":1378400400,"wish":1},{"diff":11,"end":1378429200,"start":1378407600,"wish":1},{"diff":10,"end":1378430100,"start":1378429200,"wish":1},{"diff":11,"end":1378432800,"start":1378430100,"wish":1},{"diff":10,"end":1378440000,"start":1378432800,"wish":1},{"diff":6,"end":1378443600,"start":1378440000,"wish":1},{"diff":4,"end":1378447200,"start":1378443600,"wish":1},{"diff":3,"end":1378450800,"start":1378447200,"wish":1},{"diff":4,"end":1378483200,"start":1378450800,"wish":1},{"diff":5,"end":1378485000,"start":1378483200,"wish":1},{"diff":6,"end":1378486800,"start":1378485000,"wish":1},{"diff":7,"end":1378494000,"start":1378486800,"wish":1},{"diff":8,"end":1378497600,"start":1378494000,"wish":1},{"diff":7,"end":1378530000,"start":1378497600,"wish":1},{"diff":6,"end":1378533600,"start":1378530000,"wish":1},{"diff":5,"end":1378569600,"start":1378533600,"wish":1},{"diff":4,"end":1378573200,"start":1378569600,"wish":1},{"diff":5,"end":1378591200,"start":1378573200,"wish":1},{"diff":6,"end":1378598400,"start":1378591200,"wish":1},{"diff":5,"end":1378616400,"start":1378598400,"wish":1},{"diff":4,"end":1378641600,"start":1378616400,"wish":1},{"diff":5,"end":1378659600,"start":1378641600,"wish":1},{"diff":6,"end":1378670400,"start":1378659600,"wish":1},{"diff":7,"end":1378677600,"start":1378670400,"wish":1},{"diff":8,"end":1378692000,"start":1378677600,"wish":1},{"diff":7,"end":1378699200,"start":1378692000,"wish":1},{"diff":4,"end":1378702800,"start":1378699200,"wish":1},{"diff":3,"end":1378706400,"start":1378702800,"wish":1},{"diff":2,"end":1378738800,"start":1378706400,"wish":1},{"diff":3,"end":1378740600,"start":1378738800,"wish":1},{"diff":4,"end":1378746000,"start":1378740600,"wish":1},{"diff":6,"end":1378749600,"start":1378746000,"wish":1},{"diff":7,"end":1378753200,"start":1378749600,"wish":1},{"diff":6,"end":1378760400,"start":1378753200,"wish":1},{"diff":9,"end":1378764000,"start":1378760400,"wish":1},{"diff":10,"end":1378778400,"start":1378764000,"wish":1},{"diff":9,"end":1378785600,"start":1378778400,"wish":1},{"diff":7,"end":1378789200,"start":1378785600,"wish":1},{"diff":3,"end":1378825200,"start":1378789200,"wish":1},{"diff":4,"end":1378827000,"start":1378825200,"wish":1},{"diff":5,"end":1378830600,"start":1378827000,"wish":1},{"diff":6,"end":1378832400,"start":1378830600,"wish":1},{"diff":9,"end":1378839600,"start":1378832400,"wish":1},{"diff":10,"end":1378850400,"start":1378839600,"wish":1},{"diff":11,"end":1378864800,"start":1378850400,"wish":1},{"diff":10,"end":1378872000,"start":1378864800,"wish":1},{"diff":7,"end":1378875600,"start":1378872000,"wish":1},{"diff":4,"end":1378879200,"start":1378875600,"wish":1},{"diff":3,"end":1378893600,"start":1378879200,"wish":1},{"diff":2,"end":1378913400,"start":1378893600,"wish":1},{"diff":3,"end":1378915200,"start":1378913400,"wish":1},{"diff":5,"end":1378917000,"start":1378915200,"wish":1},{"diff":6,"end":1378918800,"start":1378917000,"wish":1},{"diff":9,"end":1378929600,"start":1378918800,"wish":1},{"diff":8,"end":1378936800,"start":1378929600,"wish":1}],"ratios":{"less":0,"even":0,"more":100},"durations":{"less":0,"even":0,"more":604800,"total":604800}}],"synced":1378384322609,"periods":{"start":1378332000000,"end":1378936800000}};
+      // console.log('data ->', angular.toJson(data));
 
-      var data = {"user":[{"count":0,"end":1378681200,"recursive":true,"start":1378504800,"text":"com.ask-cs.State.Available","type":"availability","wish":0},{"count":0,"end":1378850400,"recursive":true,"start":1378720800,"text":"com.ask-cs.State.Available","type":"availability","wish":0}],"groupId":"a2408ffc-69b5-1030-a3ab-005056bc7e66","synced":1378384322609,"periods":{"start":1378332000000,"end":1378936800000}};
-
-      console.log('data ->', angular.toJson(data));
+      console.log('this week ->', Number(Date.today()));
 
 
       /**
