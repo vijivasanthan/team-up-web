0607b3a129485881a16aac6536903fd5e6addd6b
Cengizism <culusoy@ask-cs.com>
2014-04-30 21:05:45 +0200

    stadard code styling 2

--- app/scripts/controllers/logout.js
+++ app/scripts/controllers/logout.js
@@ -1,42 +1,42 @@
 define(
-  ['controllers/controllers'],
-  function (controllers)
-  {
-    'use strict';
+	['controllers/controllers'],
+	function (controllers)
+	{
+		'use strict';
 
-    controllers.controller(
-      'logout',
-      [
-        '$rootScope', '$scope', '$window', 'Session', 'User', 'Storage',
-        function ($rootScope, $scope, $window, Session, User, Storage)
-        {
-          $('.navbar').hide();
-          $('#footer').hide();
+		controllers.controller(
+			'logout',
+			[
+				'$rootScope', '$scope', '$window', 'Session', 'User', 'Storage',
+				function ($rootScope, $scope, $window, Session, User, Storage)
+				{
+					$('.navbar').hide();
+					$('#footer').hide();
 
-          var logindata = angular.fromJson(Storage.get('logindata'));
+					var logindata = angular.fromJson(Storage.get('logindata'));
 
-          User.logout()
-            .then(
-            function (result)
-            {
-              if (result.error)
-              {
-                console.warn('error ->', result);
-              }
-              else
-              {
-                // Storage.clearAll();
+					User.logout()
+						.then(
+						function (result)
+						{
+							if (result.error)
+							{
+								console.warn('error ->', result);
+							}
+							else
+							{
+								// Storage.clearAll();
 
-                Storage.session.clearAll();
+								Storage.session.clearAll();
 
-                Storage.add('logindata', angular.toJson(logindata));
+								Storage.add('logindata', angular.toJson(logindata));
 
-                Storage.cookie.clearAll();
-                $window.location.href = 'logout.html';
-              }
-            });
-        }
-      ]);
+								Storage.cookie.clearAll();
+								$window.location.href = 'logout.html';
+							}
+						});
+				}
+			]);
 
-  }
+	}
 );
