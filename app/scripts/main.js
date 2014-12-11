a389866325a82fd2fa2866f78017cc32659ba7b0
henk <henkiexxx4@gmail.com>
2014-12-11 09:32:46 +0100

    deploying test and changing profile in test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@ require.config(
   {
     waitSeconds: 100,
     paths: {
-      profile: 'profiles/decentrale-demo/profile',
+      profile: 'profiles/test/profile',
       date: 'removables/date',
       angular: '../vendors/angular/angular.min',
       jquery: '../vendors/jquery/dist/jquery.min',
