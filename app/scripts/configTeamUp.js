4ee6dbbb67556b7469e404eda0068d03407ca123
henk <henkiexxx4@gmail.com>
2015-08-28 16:41:46 +0200

    removed test part out of the config

--- app/scripts/configTeamUp.js
+++ app/scripts/configTeamUp.js
@@ -201,7 +201,7 @@ define(['profile'], function(profile){
   function getLanguage()
   {
     var lang = profile.lang,
-      currentHost = 'www.lala.en';//window.location.host,
+      currentHost = window.location.host,
       langs = ['nl', 'de', 'en'];
 
     langs.forEach(function (val)
