e4e7cbd6644141766a5691cb68ff05d40ad329a2
henk <henkiexxx4@gmail.com>
2015-09-07 12:03:55 +0200

    fix height bug sortable ios

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -14,11 +14,6 @@ define(
 
         $scope.currentTeam = CurrentSelection.getTeamId();
 
-        $scope.test = ["1.0", "1.3", "1.1", "1.5", "1.6", "1.7", "1.8",
-          "1.4", "1.2", "1.9", "1.10", "1.11", "1.12",
-          "1.13", "1.14", "1.15", "1.16", "1.17", "1.18",
-          "1.19"];
-
         $scope.orderTypes = [
           {
           name: $rootScope.ui.order.fixedOrder,
