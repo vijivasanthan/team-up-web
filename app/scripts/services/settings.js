5752b1cf5c92d873db8f5aec253dbed663e2e93a
henk <henkiexxx4@gmail.com>
2015-09-10 12:50:08 +0200

    Create a statuscode order, what will be decide what result will be the right one

--- app/scripts/services/settings.js
+++ app/scripts/services/settings.js
@@ -3,136 +3,99 @@ define(['services/services', 'config'], function (services, config) {
 
   services.factory('Settings', function ($q, Store, $injector) {
     return {
+      /**
+       * get the backend directory locally
+       * @returns {*} return the backend dir
+       */
       getBackEnd: function()
       {
         var backEnd = Store('app').get('backEnd');
 
-        return (backEnd && backEnd.get) ? backEnd.get : config.app.host;
+        return backEnd.get;
       },
+      /**
+       * Save the backend directory locally
+       * @param backEnd The backend directory
+       */
       setBackEnd: function(backEnd)
       {
         Store('app').save('backEnd', { 'get': backEnd });
-        return backEnd;
       },
+      /**
+       * initialise backend directory by logging in on all backends defined
+       * after that validate all the responses on statuscode
+       * in the localConfig.host
+       * @param userName
+       * @param passWord
+       * @returns {*} the response, if the response was rejected a errorMessage is added
+       */
       initBackEnd: function(userName, passWord)
       {
         var deferred = $q.defer(),
-          self = this;
+          self = this,
+          promises = [];
 
-        if(config.app.otapRole == 'dev')
+        //create http requests with the different backends
+        _.each(config.app.host, function (backEndDir)
         {
-          var promises = [];
-          //create http requests with the different backends
-          _.each(config.app.backEnds, function (backEndDir)
-          {
-            var request = self.login(backEndDir, userName, passWord);
-            promises.push(request);
-          });
+          var request = self.login(backEndDir, userName, passWord);
+          promises.push(request);
+        });
 
-          //Resolve all promises even if there are rejections
-          this.aSync(promises, function error(results)
-          {
-            console.log('Rejected request:', results);
-          }, function success(results)
-          {
-            console.log('Results (final):', results);
-            var finalIndex = null;
-            results.some(function (singleResult, index)
-            {
-              finalIndex = index;
-              singleResult = self.validate(singleResult);
-              return (singleResult['X-SESSION_ID']);
-            });
-
-            self.setBackEnd(config.app.backEnds[finalIndex]);
-            deferred.resolve(results[finalIndex]);
-          });
-
-
-          //$q.all(promises)
-          //  .then(function (results)
-          //  {
-          //    console.log('results', results);
-          //    var finalIndex = null;
-          //    //the first valid result will be the final backEnd
-          //    results.some(function (singleResult, index)
-          //    {
-          //      finalIndex = index;
-          //      //singleResult = self.validate(singleResult);
-          //      return (singleResult['X-SESSION_ID']);
-          //    });
-          //    //if none of the results is valid
-          //    //if(! results[finalIndex].validate)
-          //    //{
-          //    //  //When the system tries to login and all hosts time-out, show a message to the user that the system is temporarily unavailable and that they should try again
-          //    //  results[finalIndex].errorMessage = 'System is temporarily unavailable, try again in a minute';
-          //    //  self.setBackEnd(config.app.host);//set backend back on the default host
-          //    //} else {
-          //    //  self.setBackEnd(config.app.backEnds[finalIndex]);//set the backend on the one who was logging in correctly
-          //    //}
-          //
-          //    self.setBackEnd(config.app.backEnds[finalIndex]);
-          //    deferred.resolve(results[finalIndex]);
-          //  }, function (error)
-          //  {
-          //    console.log('error', error);
-          //  });
-        }
-        else
+        //Resolve all promises even if there are rejections
+        this.aSync(promises, function error(results)
         {
-          var self = this;
-          self.setBackEnd(config.app.host);
-
-          //There is still one backend, so only one promise to resolve
-          self.login(config.app.host, userName, passWord)
-            .then(function(result)
-            {
-              deferred.resolve(result);
-            },
-            function (error)
-            {
-              error = self.validate(error);
-              deferred.resolve(error);
-            });
-        }
+          console.log('Rejected requests:', results);
+        }, function success(results)
+        {
+          var result = self.validate(results);
+          self.setBackEnd(config.app.host[result.index]);
+          deferred.resolve(result);
+        });
         return deferred.promise;
       },
       /**
-       * Validate the result of the login
-       * @param result the result of tje login
+       * Validate all the results of the login attempts on statuscode
+       * @param result results of the logins
        * @returns {result} with the validation true or false
        */
-      validate: function(result)
+      validate: function(results)
       {
-        var $rootScope = $injector.get('$rootScope');
+        var $rootScope = $injector.get('$rootScope'),
+          statusCodes = _.pluck(results, 'status'),
+          //add a index to the result, so by returing the system will know what result was the first in order
+          index = -1;
 
-        result.validation = true;
-        //check if 404 or 200 with session
-        var status = 0;
-        if (result.status)
+        // statuscode undefined is equal to 200, because there will be not request info
+        // when there is a successfull result
+        if( (index = statusCodes.indexOf(undefined)) >= 0)
         {
-          status = result.status;
+          results[index].validate = true;
         }
-        else if (result.error && result.error.status)
+        else if( (index = statusCodes.indexOf(400)) >= 0 ||
+          (index = statusCodes.indexOf(403)) >= 0 ||
+          (index = statusCodes.indexOf(404)) >= 0 )
         {
-          status = result.error.status;
+          results[index].validate = false;
+          results[index].errorMessage = $rootScope.ui.login.alert_wrongUserPass;
         }
-        if (status == 400 ||
-          status == 403 ||
-          status == 404 ||
-          result.error)
+        else if( (index = statusCodes.indexOf(503)) >= 0)
         {
-          result.validation = false;
-          result.errorMessage = $rootScope.ui.login.alert_wrongUserPass;
-
+          results[index].validate = false;
+          results[index].errorMessage = "The system is temporarily unavailable, try again in a few seconds";
         }
-        else if (result.status == 0)
+        else if( (index = statusCodes.indexOf(0)) >= 0)
         {
-          result.validation = false;
-          result.errorMessage = $rootScope.ui.login.alert_network;
-
+          results[index].validate = false;
+          results[index].errorMessage = "The backend does not exist";
         }
-        return result;
+        else
+        {
+          results[index].validate = false;
+          results[index].errorMessage = 'Statuscode not regonized';
+        }
+        results[index].index = index;
+        return results[index];
       },
       /**
        *
@@ -156,6 +119,13 @@ define(['services/services', 'config'], function (services, config) {
           pass: passWord
         }).$promise;
       },
+      /**
+       * This method will bundle all the responses, and rejections, not seperated in a resolved and rejected callBack
+       * like $q.all does
+       * @param listOfPromises
+       * @param onErrorCallback
+       * @param finalCallback
+       */
       aSync: function (listOfPromises, onErrorCallback, finalCallback) {
 
         listOfPromises  = listOfPromises  || [];
@@ -182,117 +152,5 @@ define(['services/services', 'config'], function (services, config) {
         $q.all(newListOfPromises).then(finalCallback);
       }
     };
-
-    // var Settings = $resource();
-
-    // Settings.prototype.get = function () {
-    //   var deferred = $q.defer(),
-    //       settings = Store('app').get('settings');
-
-    //   if(settings.length)
-    //   {
-    //     deferred.resolve(settings);
-    //   }
-    //   else
-    //   {
-    //     Settings.prototype.init()
-    //       .then(
-    //         function(userSettings)
-    //         {
-    //           deferred.resolve(userSettings);
-    //         }
-    //       );
-    //   }
-
-    //   return deferred.promise;
-    // };
-
-    // /**
-    //  * Checks if the user already have some setting otherwise return and save the default settings
-    //  * @returns {*} promise with the settings of the user
-    //  */
-    // Settings.prototype.init = function()
-    // {
-    //   var deferred = $q.defer();
-
-    //   Profile.get($rootScope.app.resources.uuid)
-    //     .then(
-    //     function(profileData)
-    //     {
-    //       if(profileData)
-    //       {
-    //         var settings = Settings.prototype.default();
-
-    //         if(profileData.settings)
-    //         {
-    //           settings = profileData.settings;
-    //         }
-    //         else
-    //         {
-    //           Settings.prototype.save($rootScope.app.resources.uuid, settings);
-    //         }
-
-    //         Store('app').save('settings', settings);
-    //         deferred.resolve(settings);
-    //       }
-    //       else
-    //       {
-    //         console.error('No profile data', profileData);
-    //       }
-    //     }
-    //   );
-
-    //   return deferred.promise;
-    // };
-
-    // Settings.prototype.default = function()
-    // {
-    //   return $rootScope.config.app.settings;
-    // };
-
-    // Settings.prototype.save = function (id, settings) {
-    //   var deferred = $q.defer();
-
-    //   Profile.save(id, settings).then(function () {
-    //     deferred.resolve({ saved: true });
-    //   });
-
-    //   return deferred.promise;
-    // };
-
-    // // BackendSettings
-    // var BackendSettings = $resource(
-    //   config.host + '/settings',
-    //   {},
-    //   {
-    //     get: {
-    //       method: 'GET',
-    //       params: {}
-    //     }
-    //   }
-    // );
-
-
-    // // Get BackendSettings
-    // Settings.prototype.getBackendSettings = function () {
-    //   var deferred = $q.defer();
-
-    //   BackendSettings.get(
-    //     {},
-    //     function (results)
-    //     {
-    //       Store('BackendSettings').save('settings', results);
-    //       deferred.resolve(results);
-    //     },
-    //     function (error)
-    //     {
-    //       deferred.resolve(error);
-    //     }
-    //   );
-
-    //   return deferred.promise;
-    // };
-
-    // return new Settings;
   });
 });
