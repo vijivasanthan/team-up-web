db8a44aa7914f403438d84a8294b5011f1acc970
henk <henkiexxx4@gmail.com>
2015-09-11 15:03:10 +0200

    Changed statuscode prior method, added the param for backend directories and added some documentation to the different methods

--- app/scripts/services/settings.js
+++ app/scripts/services/settings.js
@@ -1,42 +1,45 @@
-define(['services/services', 'config'], function (services, config) {
+define(['services/services'], function (services)
+{
   'use strict';
 
-  services.factory('Settings', function ($q, Store, $injector) {
+  services.factory('Settings', function ($q, Store, $injector)
+  {
     return {
       /**
        * get the backend directory locally
        * @returns {*} return the backend dir
        */
-      getBackEnd: function()
+      getBackEnd: function ()
       {
         var backEnd = Store('app').get('backEnd');
-
+        //the default back End is for testpurposes only
         return backEnd.get;
       },
       /**
        * Save the backend directory locally
        * @param backEnd The backend directory
        */
-      setBackEnd: function(backEnd)
+      setBackEnd: function (backEnd)
       {
-        Store('app').save('backEnd', { 'get': backEnd });
+        Store('app').save('backEnd', {'get': backEnd});
       },
       /**
        * initialise backend directory by logging in on all backends defined
        * after that validate all the responses on statuscode
        * in the localConfig.host
+       * @param backEnds all the backend directories
        * @param userName
        * @param passWord
        * @returns {*} the response, if the response was rejected a errorMessage is added
        */
-      initBackEnd: function(userName, passWord)
+      initBackEnd: function (backEnds, userName, passWord)
       {
         var deferred = $q.defer(),
           self = this,
           promises = [];
 
         //create http requests with the different backends
-        _.each(config.app.host, function (backEndDir)
+        _.each(backEnds, function (backEndDir)
         {
           var request = self.login(backEndDir, userName, passWord);
           promises.push(request);
@@ -44,68 +47,96 @@ define(['services/services', 'config'], function (services, config) {
 
         //Resolve all promises even if there are rejections
         this.aSync(promises, function error(results)
+        {},
+        function success(results)
         {
-          console.log('Rejected requests:', results);
-        }, function success(results)
-        {
-          var result = self.validate(results);
-          self.setBackEnd(config.app.host[result.index]);
+          var result = self.getResultOnStatusCode(results);
+          self.setBackEnd(backEnds[result.index]);
           deferred.resolve(result);
         });
         return deferred.promise;
       },
       /**
-       * Validate all the results of the login attempts on statuscode
-       * @param result results of the logins
-       * @returns {result} with the validation true or false
+       * Depending on the order of status codes,
+       * the first result in that order will return validated
+       * @param results All the requests with the different backends
+       * @returns {*} The first result in the order of status codes will return
        */
-      validate: function(results)
+      getResultOnStatusCode: function (results)
       {
         var $rootScope = $injector.get('$rootScope'),
           statusCodes = _.pluck(results, 'status'),
-          //add a index to the result, so by returing the system will know what result was the first in order
+          self = this,
           index = 0;
 
-        // statuscode undefined is equal to 200, because there will be not request info
-        // when there is a successfull result
-        if( (index = statusCodes.indexOf(undefined)) >= 0)
+        // The order of these status codes is mandatory!
+        [undefined, 400, 403, 404, 503, 0]
+          .some(function (statusCode)
+          {
+            index = statusCodes.indexOf(statusCode);
+            if (index !== -1)
+            {
+              results[index].index = index;
+              results[index] = self.validate(results[index]);
+              return true;
+            }
+          });
+        //statuscode not found
+        if (index === -1)
         {
-          results[index].validate = true;
-        }
-        else if( (index = statusCodes.indexOf(400)) >= 0 ||
-          (index = statusCodes.indexOf(403)) >= 0 ||
-          (index = statusCodes.indexOf(404)) >= 0 )
-        {
-          results[index].validate = false;
-          results[index].errorMessage = $rootScope.ui.login.alert_wrongUserPass;
-        }
-        else if( (index = statusCodes.indexOf(503)) >= 0)
-        {
-          results[index].validate = false;
-          results[index].errorMessage = $rootScope.ui.teamup.systemUnavailable;
-        }
-        else if( (index = statusCodes.indexOf(0)) >= 0)
-        {
-          results[index].validate = false;
-          results[index].errorMessage = $rootScope.ui.teamup.noBackend;
-        }
-        else
-        {
+          index = 0;
           results[index].validate = false;
           results[index].errorMessage = $rootScope.ui.teamup.statusCodeNotRegonized;
         }
-        results[index].index = index;
+
         return results[index];
       },
       /**
-       *
-       * @param backEndDir the location of the backend
-       * @returns {*|{method, params}} returns a resource promise
+       * Validate the result on statuscode and add a error message
+       * @param result the result inclusive a key value with valid true/false and a error message
+       * if the result was not successfully
+       * @returns {result} with the validation true or false
        */
-      login: function(backEndDir, userName, passWord)
+      validate: function (result)
+      {
+        var $rootScope = $injector.get('$rootScope');
+
+        switch (result.status)
+        {
+          case undefined:
+            result.valid = true;
+            break;
+          case 0:
+            result.valid = false;
+            result.errorMessage = $rootScope.ui.teamup.noBackend;
+            break;
+
+          case 400:
+          case 403:
+          case 404:
+            result.valid = false;
+            result.errorMessage = $rootScope.ui.login.alert_wrongUserPass;
+            break;
+
+          case 503:
+            result.valid = false;
+            result.errorMessage = $rootScope.ui.teamup.systemUnavailable;
+            break;
+        }
+        return result;
+      },
+      /**
+       * Create a resource object with the combination of the backend directory, username and password
+       * and return this as a promise
+       * @param backEndDir the location of the backend
+       * @param userName
+       * @param passWord
+       * @returns {*|{method, params}} returns a promify resource
+       */
+      login: function (backEndDir, userName, passWord)
       {
         var $resource = $injector.get('$resource'),
-          login = $resource(backEndDir + 'login/',  {}, {
+          login = $resource(backEndDir + 'login/', {}, {
             get: {
               method: 'GET',
               params: {
@@ -126,16 +157,19 @@ define(['services/services', 'config'], function (services, config) {
        * @param onErrorCallback
        * @param finalCallback
        */
-      aSync: function (listOfPromises, onErrorCallback, finalCallback) {
+      aSync: function (listOfPromises, onErrorCallback, finalCallback)
+      {
 
-        listOfPromises  = listOfPromises  || [];
+        listOfPromises = listOfPromises || [];
         onErrorCallback = onErrorCallback || angular.noop;
-        finalCallback   = finalCallback   || angular.noop;
+        finalCallback = finalCallback || angular.noop;
 
         // Create a new list of promises
         // that can "recover" from rejection
-        var newListOfPromises = listOfPromises.map(function (promise) {
-          return promise.catch(function (reason) {
+        var newListOfPromises = listOfPromises.map(function (promise)
+        {
+          return promise.catch(function (reason)
+          {
 
             // First call the `onErrroCallback`
             onErrorCallback(reason);
@@ -143,7 +177,6 @@ define(['services/services', 'config'], function (services, config) {
             // Change the returned value to indicate that it was rejected
             // Based on the type of `reason` you might need to change this
             // (e.g. if `reason` is an object, add a `rejected` property)
-            console.log('rejected');
             return reason;
           });
         });
