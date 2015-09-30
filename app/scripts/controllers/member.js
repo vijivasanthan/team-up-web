1b191720982efeee6b5df1bd5bd7f670658cfedc
henk <henkiexxx4@gmail.com>
2015-09-30 14:45:42 +0200

    changed viewmodel into self

--- app/scripts/controllers/member.js
+++ app/scripts/controllers/member.js
@@ -8,18 +8,18 @@ define(
       'member',
       function ($scope, Team, Member, data)
       {
-        var vm = this;
+        var self = this;
 
         /**
          * Delete a team member from a single team
          * @param member the info of the member who will be deleted
          */
-        vm.delete = function (current, teamId, confirm)
+        self.delete = function (current, teamId, confirm)
         {
           //Confirmation by deleting a member
           if(! confirm)
           {
-            vm.current = current;
+            self.current = current;
             angular.element('#confirmMemberModal').modal('show');
           }
           else
@@ -40,20 +40,20 @@ define(
         /**
          * Remove the current team from the teams of the user in the interface,
          * because the user with the role of teammember
-         * is not permitted to see vm team anymore
+         * is not permitted to see self team anymore
          */
         function removeTeamFromView()
         {
-          vm.data.currentTeamId = vm.data.teams[0].uuid;
-          vm.requestTeam(vm.data.currentTeamId);
+          self.data.currentTeamId = self.data.teams[0].uuid;
+          self.requestTeam(self.data.currentTeamId);
         }
 
-        vm.init = function (teamId, membersData)
+        self.init = function (teamId, membersData)
         {
           if(membersData)
           {
             Member.init(membersData);
-            vm.list = Member.getList();
+            self.list = Member.getList();
           }
           else
           {
@@ -61,12 +61,12 @@ define(
               .then(function(members)
               {
                 Member.init(members);
-                vm.list = Member.getList();
+                self.list = Member.getList();
               });
           }
         };
 
-        vm.init(Team.getCurrent(), data);
+        self.init(Team.getCurrent(), data);
       }
     );
   }
--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -9,28 +9,28 @@ define(
       function (Team)
       {
         //view model
-        var vm = this;
+        var self = this;
 
         /**
          * Create a team
          * @param teamName The name of the team
          */
-        vm.create = function(teamName)
+        self.create = function(teamName)
         {
           Team.create(teamName);
         };
 
-        vm.read = function (current)
+        self.read = function (current)
         {
           Team.read(current);
         };
 
-        vm.update = function (team, confirm)
+        self.update = function (team, confirm)
         {
           if(! confirm)
           {
-            vm.updateForm = true;
-            vm.editForm = {
+            self.updateForm = true;
+            self.editForm = {
               name: team.name,
               uuid: team.uuid
             };
@@ -40,12 +40,12 @@ define(
             Team.update(team)
               .then(function()
               {
-                vm.updateForm = false;
+                self.updateForm = false;
               });
           }
         };
 
-        vm.delete = function (current, confirm, callback)
+        self.delete = function (current, confirm, callback)
         {
           if(! confirm)
           {
@@ -61,16 +61,16 @@ define(
           }
         };
 
-        vm.init = function (current, callback)
+        self.init = function (current, callback)
         {
           //set the list from the resolve method
           Team.init(current);
-          vm.list = Team.getList();
-          vm.current = Team.getCurrent();
+          self.list = Team.getList();
+          self.current = Team.getCurrent();
           (callback && callback(current));
         };
 
-        vm.init();
+        self.init();
       }
     );
   }
