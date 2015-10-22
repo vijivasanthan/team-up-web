96ec43ea3daced99aa1209834426a7badd757891
henk <henkiexxx4@gmail.com>
2015-10-22 17:59:33 +0200

    Improved validation new member add

--- app/scripts/controllers/member.js
+++ app/scripts/controllers/member.js
@@ -10,17 +10,81 @@ define(
       {
         var self = this;
 
+        self.searchResults = null; //The searchresult are by default empty
+        self.searchValue = ""; // The value of the search is empty by start
+        self.searchSubmit = false; //The submit button on the search field is default false
+        self.searchTeams = []; //The teams of the members found by the searched value
+        self.roles = Member.getRoles();
+
+        self.create = create;
+        self.delete = _delete;
+        self.checkUserName = checkUserName;
+        self.search = search;
+        self.init = init;
+
+        if (data)
+        {
+          self.init(Team.getCurrent(), data);
+        }
+
+        /**
+         * Create new member if the member data is valid
+         * @param member The new member data
+         * @param teamId The current selected team
+         */
+        function create(member, teamId)
+        {
+          if (Member.valid(member))
+          {
+            console.error('VALIDateddd');
+            //Member.create(member, teamId);
+          }
+        }
+
         /**
          * Delete a team member from a single team
          * @param member the info of the member who will be deleted
          */
-        self.delete = function (current, teamId, confirm)
+        function _delete(current, teamId, confirm)
         {
           //Confirmation by deleting a member
-          (! confirm)
+          (!confirm)
             ? angular.element('#confirmMemberModal').modal('show')
             : Member.deleteFromSingleTeam(current, teamId);
-        };
+        }
+
+        /**
+         * Search for a member by his first -and/or lastname
+         * @param value
+         */
+        function search(value)
+        {
+          self.findMembersLoad = true;
+          Member.search(value)
+            .then(function (result)
+            {
+              self.load = false;
+              self.searchResults = result.members;
+              self.searchTeams = result.teams;
+            });
+        }
+
+        /**
+         * Check if the username is correct, otherwise remove the unwanted chars
+         */
+        function checkUserName()
+        {
+          var regUserName = /([A-Za-z0-9-_])/g,
+            matchesUserName = (self.form.userName.match(regUserName));
+
+          if (!_.isNull(matchesUserName))
+          {
+            matchesUserName = matchesUserName.join('');
+          }
+
+          self.UserNameWrong = (self.form.userName !== matchesUserName);
+          self.form.userName = matchesUserName || '';
+        }
 
         /**
          * Initialize a team with members
@@ -29,7 +93,7 @@ define(
          * @param membersData members of the team already initialized
          * @param callback to read the data of a team and send the result of members back
          */
-        self.init = function (teamId, membersData, callback)
+        function init(teamId, membersData, callback)
         {
           (membersData)
             ? initialize(membersData)
@@ -40,9 +104,7 @@ define(
             Member.init(data);
             self.list = Member.getList();
           }
-        };
-
-        self.init(Team.getCurrent(), data);
+        }
       }
     );
   }
