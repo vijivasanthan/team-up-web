2c07147c18b315299d6daf6387e54f589b2828ef
henk <henkiexxx4@gmail.com>
2015-12-14 11:55:11 +0100

    Added a logout html to show the loader until it's redirected to the login

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -82,7 +82,7 @@ define(
             .when(
             '/logout',
             {
-              //templateUrl: 'views/logout.html',
+              templateUrl: 'views/logout.html',
               resolve: {
                 data: [
                   '$rootScope',
