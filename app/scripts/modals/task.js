724810abe15f3b0bdd67a97512df5f9523504c71
henk <henkiexxx4@gmail.com>
2016-01-19 11:47:22 +0100

    temp created a transform repsonse incase of the response is not a array for the endpoint tasks.mine

--- app/scripts/modals/task.js
+++ app/scripts/modals/task.js
@@ -1,145 +1,161 @@
 define(
   ['services/services', 'config'],
-  function (services, config)
+  function(services, config)
   {
     'use strict';
 
     services.factory(
       'Task',
-        function ($rootScope, $resource, $q, Settings)
-        {
-          var Task = $resource(
-            Settings.getBackEnd() + config.app.namespace + 'tasks/:id/:teamId', {},
-            {
-              create: { method: 'POST', params: {} },
-              read: { method: 'GET', params: {}, isArray: true },
-              update: { method: 'PUT', params: {} },
-              delete: { method: 'DELETE', params: {} }
-            });
-
-          (function()
+      function($rootScope, $resource, $q, Settings)
+      {
+        var Task = $resource(
+          Settings.getBackEnd() + config.app.namespace + 'tasks/:id/:teamId', {},
           {
-            this.create = create;
-            this.read = read;
-            this.update = update;
-            this.delete = _delete;
-
-            this.mine = mine;
-            this.range = range;
-            this.team = team;
-            this.get = get;
-
-            /**
-             * Create a task
-             * @param task The newly created task
-             * @returns {*}
-             */
-            function create(task)
-            {
-              return Task.create(
-                null, task
-              ).$promise;
-            }
-
-            /**
-             * Read specific task(s)
-             * @param params
-             * @returns {*}
-             */
-            function read(params)
-            {
-              return $q(function (resolve)
+            create: {method: 'POST', params: {}},
+            read: {
+              method: 'GET',
+              params: {},
+              transformResponse: function(data)
               {
-                Task.read(params,
-                function (result)
-                {
-                  result = JSON.parse(angular.toJson(result));
-                  resolve(result);
-                })
-              });
-            }
+                data = angular.fromJson(data);
+                console.error("data ->", data);
+                //TODO remove this if it's fixed in the backend
+                //check if is no tasks is responded in a object
+                return (data &&
+                        data.result &&
+                        data.result === "No tasks")
+                  ? []//create empty array as response
+                  : data;
+              },
+              isArray: true
+            },
+            update: {method: 'PUT', params: {}},
+            delete: {method: 'DELETE', params: {}}
+          });
 
-            /**
-             * Update a specific task
-             * @param params
-             * @returns {*}
-             */
-            function update(taskId, taskData)
-            {
-              return Task.update(
-                {id: taskId},
-                taskData
-              ).$promise;
-            }
+        (function()
+        {
+          this.create = create;
+          this.read   = read;
+          this.update = update;
+          this.delete = _delete;
 
-            /**
-             * Update a specific task
-             * @param params
-             * @returns {*}
-             */
-            function _delete(taskId)
-            {
-              return Task.delete({
-                id: taskId
-              }).$promise;
-            }
+          this.mine  = mine;
+          this.range = range;
+          this.team  = team;
+          this.get   = get;
 
-            /**
-             * Get all tasks of the logged user
-             * @returns {*}
-             */
-            function mine(statuses)
-            {
-              return read({
-                id: 'mine',
-                "statuses": statuses || '1,2'
-              });
-            }
+          /**
+           * Create a task
+           * @param task The newly created task
+           * @returns {*}
+           */
+          function create(task)
+          {
+            return Task.create(
+              null, task
+            ).$promise;
+          }
 
-            /**
-             * Get tasks by range
-             * @param range object with key value
-             * start and end
-             * @returns {*}
-             */
-            function range(range)
-            {
-              return read({
-                start: range.start,
-                end: range.end
-              });
-            }
+          /**
+           * Read specific task(s)
+           * @param params
+           * @returns {*}
+           */
+          function read(params)
+          {
+            return $q(function(resolve)
+                      {
+                        Task.read(params,
+                                  function(result)
+                                  {
+                                    result = JSON.parse(angular.toJson(result));
+                                    resolve(result);
+                                  })
+                      });
+          }
 
-            /**
-             * Get tasks by team
-             * @param teamId The id of the team
-             * @returns {*}
-             */
-            function team(teamId, statuses)
-            {
-              return read({
-                "id": 'team',
-                "teamId": teamId,
-                "statuses": statuses || '1,2'
-              });
-            }
+          /**
+           * Update a specific task
+           * @param params
+           * @returns {*}
+           */
+          function update(taskId, taskData)
+          {
+            return Task.update(
+              {id: taskId},
+              taskData
+            ).$promise;
+          }
 
-            /**
-             * Get task by id
-             * @param taskId The id of the task
-             * @returns {*}
-             */
-            function get(taskId)
-            {
-              return Task.get({
-                id: taskId
-              }).$promise;
-            }
+          /**
+           * Update a specific task
+           * @param params
+           * @returns {*}
+           */
+          function _delete(taskId)
+          {
+            return Task.delete({
+                                 id: taskId
+                               }).$promise;
+          }
 
-          }).call(Task.prototype);
+          /**
+           * Get all tasks of the logged user
+           * @returns {*}
+           */
+          function mine(statuses)
+          {
+            return read({
+                          id: 'mine',
+                          "statuses": statuses || '1,2'
+                        });
+          }
 
-          return new Task;
-        }
+          /**
+           * Get tasks by range
+           * @param range object with key value
+           * start and end
+           * @returns {*}
+           */
+          function range(range)
+          {
+            return read({
+                          start: range.start,
+                          end: range.end
+                        });
+          }
+
+          /**
+           * Get tasks by team
+           * @param teamId The id of the team
+           * @returns {*}
+           */
+          function team(teamId, statuses)
+          {
+            return read({
+                          "id": 'team',
+                          "teamId": teamId,
+                          "statuses": statuses || '1,2'
+                        });
+          }
+
+          /**
+           * Get task by id
+           * @param taskId The id of the task
+           * @returns {*}
+           */
+          function get(taskId)
+          {
+            return Task.get({
+                              id: taskId
+                            }).$promise;
+          }
+
+        }).call(Task.prototype);
+
+        return new Task;
+      }
     );
   }
 );
