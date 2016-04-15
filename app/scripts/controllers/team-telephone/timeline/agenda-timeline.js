8e0ad25944122f3d8539df0cda3e4853772a5c24
henk <henkiexxx4@gmail.com>
2016-04-15 11:59:04 +0200

    wish validation fix

--- app/scripts/controllers/team-telephone/timeline/agenda-timeline.js
+++ app/scripts/controllers/team-telephone/timeline/agenda-timeline.js
@@ -2464,6 +2464,10 @@ define(
          */
         $scope.wisher = function (slot)
         {
+          var wishAmount = parseInt(slot.wish);
+          if(! (wishAmount >= 0 && wishAmount <= 30) )
+            return $rootScope.notifier.error($rootScope.ui.validation.wish.integer);
+
           $rootScope.statusBar.display($rootScope.ui.agenda.changingWish);
 
           var formattedSlot = {
